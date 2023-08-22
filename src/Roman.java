188f2e0d338dd1183717d57c2899b850cb30ae7c
n.ponik <n.ponik@localhost>
2023-08-22 20:36:15 +0300

    Fixed bug with converting numbers to Roman add values in enum

--- src/Roman.java
+++ src/Roman.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 enum Roman {
-    I(1), IV(4), V(5), IX(9), X(10);
+    I(1), IV(4), V(5), IX(9), X(10),
+    XL(40), L(50), XC(90), C(100),
+    CD(400), D(500), CM(900), M(1000);
     private final int value;
 
     Roman(int value) {
